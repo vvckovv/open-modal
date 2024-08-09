80d7d55d0bfa7946f26e0cf6e8a2e5bb66539841
Opeyemi <156693393+vvckovv@users.noreply.github.com>
2024-08-09 15:06:32 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let modalBtn = document.getElementById('modal-btn');
+
+function openModal(){
+    popup.classList.add("open-popup");
+}
+
+function closeModal(){
+    popup.classList.remove("open-popup");
+}
